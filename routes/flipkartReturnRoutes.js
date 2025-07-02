e9ce8c1a12976c2874199cd987ae232bc0cacff2
mohitraipr <140521478+mohitraipr@users.noreply.github.com>
2025-07-02 11:56:58 +0530

    Update flipkartReturnRoutes.js

--- routes/flipkartReturnRoutes.js
+++ routes/flipkartReturnRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios   = require('axios');
-const { getAccessToken } = require('./flipkartAuth');
+const { getAccessToken } = require('../flipkartAuth');
 
 const router = express.Router();
 const SOURCE = 'courier_return';
