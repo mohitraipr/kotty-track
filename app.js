a01c0abbc1e26b3c93274c2c4f9841f3eecb1bb9
mohitraipr <140521478+mohitraipr@users.noreply.github.com>
2025-12-23 15:24:46 +0530

    Fix professional.css static route

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Serve Static Files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Global Variables for Views
